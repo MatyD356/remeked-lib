6f4d479431335cb7ee7e7cf4d005bb4816faef6a
Mateusz <matyd356@gmail.com>
2020-07-06 17:47:08 +0200

    add Aside component

---
+++ src/Aside/Aside.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Aside.css'
+
+class Aside extends React.Component {
+  render() {
+    return (
+      <div className="Aside" />
+    );
+  };
+};
+
+export default Aside;

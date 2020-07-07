acd5a67c36be732f9f6d36aa7e821e40baae80ef
Mateusz <matyd356@gmail.com>
2020-07-07 18:38:58 +0200

    add Book component

---
+++ src/Book/Book.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Book extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      author: '',
+      length: 0,
+      status: true
+    }
+  }
+  render() {
+    return
+  }
+}
+export default Book;

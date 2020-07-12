4335607cb12e70bef1b97f912efa1aa7494591e4
Mateusz <matyd356@gmail.com>
2020-07-12 22:42:48 +0200

    add new font

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import './App.css';
+import Aside from '../Aside/Aside';
+import Main from '../Main/Main';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      books: []
+    }
+  }
+  addNewBook = () => {
+    console.log('added');
+
+  }
+  render() {
+    return (
+      <div className="App" >
+        <Aside
+          addNewBook={this.addNewBook}
+        />
+        <Main
+          books={this.state.books}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;

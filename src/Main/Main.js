e10ad2501f0b862aa319918d5cf5d0537e262d80
Mateusz <matyd356@gmail.com>
2020-07-14 21:18:10 +0200

    allow acces to store and rendering books from there

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import './Main.css'
+import './Main.scss'
+import Book from '../Book/Book'
+import { connect } from 'react-redux';
 
-function Main(props) {
-  return (
-    <main className="Main"></main>
-  )
+const mapStateToProps = (state) => {
+  return {
+    books: state.books
+  };
+};
+
+class Main extends React.Component {
+  render() {
+    return (
+      <main className="Main">
+        {this.props.books.map((item, index) => {
+          console.log(item)
+          return <Book
+            key={index}
+            name={item.name}
+            author={item.author}
+            length={item.length}
+            status={item.status}
+          />
+        })}
+      </main>
+    )
+  }
 }
 
-export default Main;
+export default connect(mapStateToProps, null)(Main);

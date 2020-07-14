1c9a081a7e9c8dbf983020c96c1dbc90687734ba
Mateusz <matyd356@gmail.com>
2020-07-14 21:21:42 +0200

    create a stateless component rendering books data

--- src/Book/Book.js
+++ src/Book/Book.js
@@ -1,19 +1,18 @@
 import React from 'react';
+import './Book.scss';
 
-class Book extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      name: '',
-      author: '',
-      length: '',
-      status: '',
-    }
-  }
-  render() {
-    return (
-      <div className="Book"></div>
-    )
-  }
+const Book = (props) => {
+  return (
+    <div className="Book">
+      <h2 className="Book-title">{props.name}</h2>
+      <p className="Book-author">Author: {props.author}</p>
+      <p className="Book-lenght">Pages: {props.length}</p>
+      <p className="Book-status">Readed: {props.status}</p>
+      <div className="Book-btn-container">
+        <button className="Book-btn change">Change Status</button>
+        <button className="Book-btn">Delete book</button>
+      </div>
+    </div>
+  )
 }
 export default Book;

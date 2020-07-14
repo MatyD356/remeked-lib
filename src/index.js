e4cd1a8ae574fb30dc4df5747f0634c5f6c07e52
Mateusz <matyd356@gmail.com>
2020-07-14 22:18:16 +0200

    add redux store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
+
+
+const initialState = {
+  books: [{ name: "LOTR", author: "Tolkien", length: 120, status: "Yes" }]
+}
+
+const booksReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD":
+      return {
+        books: [...state.books, action.payload]
+      }
+    case "DELETE":
+      return {
+        state: []
+      }
+    default:
+      return state
+  }
+};
+
+const store = createStore(booksReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );

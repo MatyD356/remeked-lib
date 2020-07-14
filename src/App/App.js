e963c696c4798c9e23c168a55fb5e6b39b95f31e
Mateusz <matyd356@gmail.com>
2020-07-14 22:16:26 +0200

    change to stateless component

--- src/App/App.js
+++ src/App/App.js
@@ -1,31 +1,15 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import Aside from '../Aside/Aside';
 import Main from '../Main/Main';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      books: []
-    }
-  }
-  addNewBook = () => {
-    console.log('added');
-
-  }
-  render() {
-    return (
-      <div className="App" >
-        <Aside
-          addNewBook={this.addNewBook}
-        />
-        <Main
-          books={this.state.books}
-        />
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="App" >
+      <Aside />
+      <Main />
+    </div>
+  );
 }
 
 export default App;
